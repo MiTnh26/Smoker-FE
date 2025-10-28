4a22e81a6571e011137c861fcba8162874a21332
dng0021 <anhle200w@gmail.com>
2025-10-29 00:39:45 +0700

    done sotry

--- src/routes/BarRoutes.js
+++ src/routes/BarRoutes.js
@@ -4,7 +4,8 @@ import BarLayout from "../layouts/BarLayout";
 import BarProfile from "../modules/bar/pages/BarProfile";
 import ProtectedRoute from "./ProtectedRoute"; // import ProtectedRoute
 import BarSettings from "../modules/bar/pages/BarSettings";
-
+import ManagePost from "../modules/bar/pages/ManagePost";
+import ManageStory from "../modules/bar/pages/ManageStory";
 export default function BarRoutes() {
   return (
     <Fragment>
@@ -22,9 +23,24 @@ export default function BarRoutes() {
         path="/bar/settings/:barPageId"
         element={
           <ProtectedRoute roles={["bar"]}>
-          
             <BarLayout>  <BarSettings /></BarLayout>
           </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/bar/manage-post"
+        element={
+          // <ProtectedRoute roles={["bar"]}>
+            <BarLayout><ManagePost /></BarLayout>
+        //  </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/bar/manage-story"
+        element={
+          // <ProtectedRoute roles={["bar"]}>
+            <BarLayout><ManageStory /></BarLayout>
+          // </ProtectedRoute>
         }
       />
     </Fragment>
